44b2613c5d15ff29157669a5a0c40a8f4f6231ee
Stanislau <atrohov97@gmail.com>
2024-05-09 20:54:27 +0300

    new config for vagrant

--- db_sql/slave2.sql
+++ db_sql/slave2.sql
@@ -1,7 +1,1 @@
-stop slave;
 CHANGE MASTER TO MASTER_HOST='192.168.56.2', MASTER_USER='repl', MASTER_PASSWORD='secret', GET_MASTER_PUBLIC_KEY=1;
-start slave;
-stop slave;
-reset slave;
-reset master;
-start slave;
